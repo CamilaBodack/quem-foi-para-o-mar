8f42821726d65f1814a98fb0fbeed42e20d4e405
CamilaBodack <camila.bodack@gmail.com>
2020-08-12 11:28:09 -0300

    renomeia arquivo html e cria serializers dos models

---
+++ quem_foi_para_mar_core/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+from .models import Pescador, Embarcacao, Viagem, Contato
+
+
+class PescadorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Pescador
+        fields = ("nome", "endereco", "telefone", "email")
+
+
+class EmbarcacaoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Embarcacao
+        fields = ("nome", "numeracao", "modelo", "caracteristicas")
+
+
+class ViagemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Viagem
+        fields = ("destino", "data_partida", "data_chegada_prevista",
+                  "tripulacao", "embarcacao_retornou")
+
+
+class ContatoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Contato
+        fields = ("nome", "endereco", "email", "telefone")
--- quem_foi_para_mar_core/views.py
+++ quem_foi_para_mar_core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
+from .models import Pescador, Embarcacao, Viagem, Contato
 
 
 def list_barcos(request):
-    return render(request, 'quem_foi_para_mar_core/list_barcos.html', {})
+    return render(request, 'quem_foi_para_mar_core/geral.html', {})
