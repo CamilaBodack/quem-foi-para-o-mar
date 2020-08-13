46ec45915e0532e6fe32a2b5226aa275c4446d66
CamilaBodack <camila.bodack@gmail.com>
2020-08-13 08:03:17 -0300

    atualiza nome do template geral para informativos e cria o base template

--- quem_foi_para_mar_core/views.py
+++ quem_foi_para_mar_core/views.py
@@ -5,6 +5,8 @@ from .models import Pescador, Embarcacao, Viagem, Contato
 from .serializers import (PescadorSerializer, EmbarcacaoSerializer,
                           ViagemSerializer, ContatoSerializer)
 
+from rest_framework import status
+from rest_framework.decorators import api_view
 
 class PescadorViewSet(viewsets.ModelViewSet):
     queryset = Pescador.objects.all()
@@ -20,9 +22,10 @@ class ViagemViewSet(viewsets.ModelViewSet):
     queryset = Viagem.objects.all()
     serializer_class = ViagemSerializer
 
+    @api_view(['GET'])
     def list_barcos(request):
         barcos_no_mar_hoje = Viagem.objects.filter(data_partida=timezone.now()).order_by('data_partida')
-        return render(request, 'quem_foi_para_mar_core/geral.html', {'barcos_no_mar_hoje': barcos_no_mar_hoje})
+        return render(request, 'quem_foi_para_mar_core/informativos.html', {'barcos_no_mar_hoje': barcos_no_mar_hoje})
 
 
 class ContatoViewSet(viewsets.ModelViewSet):
