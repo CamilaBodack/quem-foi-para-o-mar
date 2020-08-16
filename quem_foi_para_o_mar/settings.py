779b0b3a204470f018e08a5713c21cc389e2b6f5
CamilaBodack <camila.bodack@gmail.com>
2020-08-16 15:51:06 -0300

    muda pasta static para staticfiles

--- quem_foi_para_o_mar/settings.py
+++ quem_foi_para_o_mar/settings.py
@@ -133,5 +133,5 @@ LOGIN_URL = 'login'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = "/static/"
