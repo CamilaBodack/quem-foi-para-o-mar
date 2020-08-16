da1f759feb06d9faec6785a4196bb35aa6290f93
CamilaBodack <camila.bodack@gmail.com>
2020-08-16 16:35:44 -0300

    atualiza settings

--- quem_foi_para_o_mar/settings.py
+++ quem_foi_para_o_mar/settings.py
@@ -130,6 +130,6 @@ LOGIN_URL = 'login'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR(subdir,'static')
+STATIC_ROOT = os.path.join(BASE_DIR('static'))
 print(STATIC_ROOT)
 STATIC_URL = "/static/"
