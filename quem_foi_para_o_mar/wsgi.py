d3596fecd1844296778865bbe37950118c99ebcb
CamilaBodack <camila.bodack@gmail.com>
2020-08-17 12:06:29 -0300

    modifica o wsgi

--- quem_foi_para_o_mar/wsgi.py
+++ quem_foi_para_o_mar/wsgi.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 
 import os
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
+# Heroku: from dj_static import Cling
 
 
-application = Cling(get_wsgi_application())
+# Heroku: application = Cling(get_wsgi_application())
+
+application = get_wsgi_application()
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'quem_foi_para_o_mar.settings')
